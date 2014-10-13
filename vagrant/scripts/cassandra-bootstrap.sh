361c7700c6cc364c6dd1f62ac1f0ed10e941f4ce
Kyle Shenk <kyle.shenk@theresumator.com>
2014-10-12 23:05:57 -0400

    updated - tmp means for installing cassandra on centos7 x86_64

--- vagrant/scripts/cassandra-bootstrap.sh
+++ vagrant/scripts/cassandra-bootstrap.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-java_file="jre-7u67-linux-i586.rpm"
+## Add this hostname to the end of the ipv4 localhost line in /etc/hosts
+_hostname="$(hostname)"
+[ -z "$(egrep "$_hostname$" /etc/hosts)" ] && {
+    sed -i '/^127\.0\.0\.1/ s/$/ '$_hostname'/' /etc/hosts
+}
 
-java_install="/opt/support/$java_file"
+## TODO: works for now, but currently using a 64bit VM...
+yum -y install glibc-2.17-55.el7_0.1.i686 libgcc-4.8.2-16.2.el7_0.i686
 
-cp "$java_install" /home/$java_file
-
-yum -y install /home/$java_file
+[ -f "/opt/support/jre-7u67-linux-i586.rpm" ] && {
+    rpm -ivh /opt/support/jre-7u67-linux-i586.rpm || {
+        echo "Java Installation failed" && exit 1
+    }
+}
 
 yum -y install jna
 
